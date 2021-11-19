b9cd65c53180a2f05f98593ef6f5294cb113b232
Aakash Singh <42798540+akash2140@users.noreply.github.com>
2021-11-19 17:21:10 +0530

    Add files via upload
    Project Build using MAVEN.
    Java version Used 11.0
    Spring Boot Version 2.5.2
    Spring and Maven dependecies added in pom.xml.
    Logging : Apache commons logging.
    Concepts applied: Transactional coding,

---
+++ SpringORM_CRUD/src/main/java/com/akash/SpringOrmCrudApplication.java
@@ -0,0 +1,96 @@
+package com.akash;
+
+import java.time.LocalDate;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import com.akash.Exception.BankException;
+import com.akash.dto.CustomerDTO;
+import com.akash.dto.CustomerType;
+import com.akash.service.CustomerService;
+
+@SpringBootApplication
+public class SpringOrmCrudApplication implements CommandLineRunner {
+
+	@Autowired
+	CustomerService serv;
+
+	@Autowired
+	Environment env;
+
+	private static final Log logger = LogFactory.getLog(SpringOrmCrudApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOrmCrudApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+//		// TODO Auto-generated method stub
+		// Reading customer details with the id, Uncomment these below lines
+		//getCustomer();
+
+		// for adding customer with details, uncomment the below line
+		// addCustomer();
+
+		// Deleting using customer ID
+		// deleteCustomer();
+
+		// Update CustomerDetails
+		//updateCustomer();
+	}
+	
+	public void getCustomer()
+	{
+		try {
+			CustomerDTO dto=serv.getCustomer(12);
+			logger.info(dto);
+		}
+		catch(Exception e)
+		{
+			logger.info(env.getProperty(e.getMessage()));
+		}
+
+	}
+
+	public void addCustomer() throws Exception {
+		try {
+			CustomerDTO dto = new CustomerDTO();
+			dto.setCustomerId(2);
+			dto.setCustomerType(CustomerType.GOLD);
+			dto.setDateOfBirth(LocalDate.of(1997, 04, 03));
+			dto.setEmailId("aakash@domain.com");
+			dto.setName("Aakash Singh");
+			Integer customerId = serv.addCustomer(dto);
+			logger.info(customerId);
+		} catch (Exception e) {
+			logger.info(env.getProperty(e.getMessage()));
+		}
+	}
+
+	public void deleteCustomer() {
+		try {
+			Integer customerId = serv.deleteCustomer(2);
+			logger.info("Customer deleted with customer Id " + customerId);
+		} catch (Exception e) {
+			logger.info(env.getProperty(e.getMessage()));
+		}
+	}
+
+	private void updateCustomer() {
+		// TODO Auto-generated method stub
+		try {
+			Integer customerId=serv.updateCustomer(11, "martin@ses.com");
+			logger.info("Customer with customer Id "+customerId +" updated");
+		} catch (Exception e) {
+				logger.info(env.getProperty(e.getMessage()));
+		}
+	}
+}
---
+++ SpringORM_CRUD/src/main/java/com/akash/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.akash.service;
+
+import com.akash.Exception.BankException;
+import com.akash.dto.CustomerDTO;
+
+public interface CustomerService
+{
+	public CustomerDTO getCustomer(Integer customerId)throws BankException;
+	public Integer addCustomer(CustomerDTO dto)throws BankException;
+	public Integer deleteCustomer(Integer customerId)throws BankException;
+	public Integer updateCustomer(Integer customerId,String emailId)throws BankException;
+}
